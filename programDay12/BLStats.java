4ef11dd2e654aa76dbed7e54c33f425cbf360724
Cs07041997 <84439789+Cs07041997@users.noreply.github.com>
2021-09-27 08:09:57 +0530

    Day12

---
+++ programDay12/BLStats.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.programDay12;
+
+public class BLStats {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ programDay12/BlStdln.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.programDay12;
+
+public class BlStdln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
